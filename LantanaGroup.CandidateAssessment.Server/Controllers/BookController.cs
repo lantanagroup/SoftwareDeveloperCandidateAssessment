9581741e5a659a26a44ed0ab538683202e3fd07f
Steven Williams <steven.williams@lantanagroup.com>
2024-08-28 10:22:11 -0400

    Remove doc comments

--- LantanaGroup.CandidateAssessment.Server/Controllers/BookController.cs
+++ LantanaGroup.CandidateAssessment.Server/Controllers/BookController.cs
@@ -13,59 +13,30 @@ public class BookController : ControllerBase
         _bookRepository = bookRepository;
     }
 
-    /// <summary>
-    /// Get all books
-    /// </summary>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
     {
         throw new NotImplementedException();
     }
 
-    /// <summary>
-    /// Get a book by id
-    /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
     [HttpGet("{id}")]
     public async Task<ActionResult<Book>> GetBook(Guid id)
     {
         throw new NotImplementedException();
     }
 
-    /// <summary>
-    /// Add a book
-    /// </summary>
-    /// <param name="book"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
     [HttpPost]
     public async Task<ActionResult<Book>> AddBook([FromBody] Book book)
     {
         throw new NotImplementedException();
     }
 
-    /// <summary>
-    /// Update a book
-    /// </summary>
-    /// <param name="id"></param>
-    /// <param name="book"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
     [HttpPut("{id}")]
     public async Task<ActionResult<Book>> UpdateBook(Guid id, [FromBody] Book book)
     {
         throw new NotImplementedException();
     }
 
-    /// <summary>
-    /// Delete a book
-    /// </summary>
-    /// <param name="id"></param>
-    /// <exception cref="NotImplementedException"></exception>
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteBook(Guid id)
     {
